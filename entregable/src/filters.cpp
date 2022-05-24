1ed7fc9ae5b93b4e4dd5cc560fbcbc3e1e4ef292
ezecnl <ecoronel@alumno.huergo.edu.ar>
2022-05-24 15:52:53 -0300

    Funcion truncate agregada

--- entregable/src/filters.cpp
+++ entregable/src/filters.cpp
@@ -16,6 +16,13 @@ using namespace std;
 // COMPLETAR :)
 
 // Filtro plano como ejemplo
+void truncate(int& num)
+{
+	if(num > 255)
+	{num=255; }
+	if (num < 0)
+	{num = 0; }
+}
 
 void plain(ppm& img, unsigned char c)
 {
@@ -48,27 +55,19 @@ void blackWhite(ppm& img)
 }
 void contrast(ppm& img, float contrast)
 {
-	float x = (259.0 * (contrast + 255.0)) / (255.0 * (259.0 - contrast));
+	
 	for(int i = 0; i < img.height; i++)
 	{
 		for(int j = 0; j < img.width; j++)
 		{	
+			float x = (259.0 * (contrast + 255.0)) / (255.0 * (259.0 - contrast));
 			int r   = ((x * (img.getPixel(i,j).r - 128)) + 128);
 			int g = ((x * (img.getPixel(i,j).g - 128)) + 128);
 			int b  = ((x * (img.getPixel(i,j).b - 128)) + 128);
-			if(r > 255)
-			{ r=255;}
-			if(g > 255)
-			{ g=255;}
-			if(b > 255)
-			{ b=255;}
-
-			if(r < 0)
-			{ r=0;}
-			if(g < 0)
-			{ g=0;}
-			if(b < 0)
-			{ b=0;}
+			truncate(r);
+			truncate(g);
+			truncate(b);
+			
 
 			img.setPixel(i,j,pixel(r,g,b));
 		}			
